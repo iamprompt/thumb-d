4ed8e8cbf459b91513aa83685ffa55025f9efeef
wearedprompt <wearedprompt@gmail.com>
2021-04-02 22:26:28 +0700

    Fragment _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
