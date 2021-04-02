e8af652523be98ead0f3448dc99f3c2f3ced83de
wearedprompt <wearedprompt@gmail.com>
2021-04-02 20:03:52 +0700

    Add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class ThumbDDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default ThumbDDocument
