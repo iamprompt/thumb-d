c3e78c740b30a7e3cdd3d10ff7151f8f6cab3edc
wearedprompt <wearedprompt@gmail.com>
2021-04-12 13:30:04 +0700

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { AuthProvider } from '@/utils/auth'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" type="svg" />
+      </Head>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
