9ef5142f3a9cd36102444e8f5fb758667b6c23a6
wearedprompt <wearedprompt@gmail.com>
2021-04-12 12:24:07 +0700

    Fix order layout

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -25,7 +25,6 @@ const OrderStatus = ({ order }: { order: orderTrack[] }) => {
   return (
     <Layout nav>
       <div className="p-5 min-h-screen pb-24">
-        <BackButton />
         <div className="mt-14 mb-10 text-center">
           <div className="text-4xl font-bold">Order Status</div>
         </div>
